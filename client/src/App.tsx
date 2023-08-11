48f1866515cd098760266f9267f1fd439c0739f5
kimmand0o0 <yuwang0130@gmail.com>
2023-08-11 16:18:22 +0900

    :truck: Rename : Footer 컴포넌트 파일 및 폴더 명 변경
    - Layout 폴더명을 Footer 로 수정하였습니다.
    - Footer.tsx 파일명을 index.tsx 로 수정하였습니다.
    
    Issues : #10

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Footer from "./components/layouts/Footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
