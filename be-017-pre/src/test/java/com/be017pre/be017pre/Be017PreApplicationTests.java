493cb1ec908090ed16eaecba322747128e62bb73
KSJ-0 <kim.mail1124@gmail.com>
2023-08-11 11:48:45 +0900

    Feat:개발 환경 세팅

---
+++ be-017-pre/src/test/java/com/be017pre/be017pre/Be017PreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be017pre.be017pre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be017PreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
