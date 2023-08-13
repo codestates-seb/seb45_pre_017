60afd47eabcaa41af5d26e2d47b93ee4bac42beb
udaeng8286 <udaenglvup@gmail.com>
2023-08-14 00:25:43 +0900

    :lipstick: Design : Header 레이아 - 로고 넣기 -검색창 (기능 x) -오른쪽 nav 버튼 로그인 전 UI 구현 Issue : #4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,27 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <Header />
       <Wrapper>개발환경 셋팅 완료</Wrapper>
       <FooterBox />
     </>
   );
 }
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+`;
 
 const Wrapper = styled.div`
   height: auto;
