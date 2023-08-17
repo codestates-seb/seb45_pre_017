2c945b8a9776cf8b41d2ec023c24305044f50866
udaeng8286 <udaenglvup@gmail.com>
2023-08-18 02:31:48 +0900

    :tada: Feat : 회원가입 컴포넌트 페이지 파일 생성 Issue : #7

---
+++ client/src/components/SignUp/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const SignUp = () => {
+  return (
+    <Signup>
+      <></>
+    </Signup>
+  );
+};
+
+export default SignUp;
+
+const Signup = styled.main`
+  max-width: 100%;
+`;
---
+++ client/src/page/SignUpPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import { styled } from "styled-components";
+
+const SignUpPage = () => {
+  return <body></body>;
+};
+
+export default SignUpPage;
