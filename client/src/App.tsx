6324f570a8fc3d2831183040204df2e485a2605b
udaeng8286 <udaenglvup@gmail.com>
2023-08-21 16:34:07 +0900

    :fire: Remove : PR을 위해 파일 임시 삭제 Isuue : #6

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 import Mainpage from "./page/Mainpage";
 import QuestionContentPage from "./page/QuestionContentPage";
 import SignUpPage from "./page/SignUpPage";
-import LoginPage from "./components/Login";
+// import LoginPage from "./components/Login";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
             <Route path="/" element={<Mainpage />} />
             <Route path="/page/:votes" element={<QuestionContentPage />} />
             <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            {/* <Route path="/login" element={<LoginPage />} /> */}
           </Routes>
         </Wrapper>
       </Router>
