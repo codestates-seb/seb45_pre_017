0981fb8fd79a9aa98a9017c797d03c86bd70d25f
KSJ-0 <kim.mail1124@gmail.com>
2023-08-22 16:29:58 +0900

    Fix : cors 에러 수정

---
+++ be-017-pre/src/main/java/com/be017pre/be017pre/WebConfig.java
@@ -0,0 +1,18 @@
+package com.be017pre.be017pre;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080","http://localhost:3030","http://database-017.cjdmzjfnvqzm.ap-northeast-2.rds.amazonaws.com:8080")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization");
+    }
+}
